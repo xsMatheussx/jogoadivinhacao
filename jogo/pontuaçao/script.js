f68496c9090c25c43092d1369978b82e16cab3b9
xsMatheussx <136374363+xsMatheussx@users.noreply.github.com>
2024-06-22 10:53:08 -0300

    Add files via upload

---
+++ jogo/pontuaçao/script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var parabens = localStorage.getItem('nomeusuario');
+    document.getElementById('parabens').textContent = parabens;
+    var tentativas = localStorage.getItem('tentativas');
+    document.getElementById('tentativas').textContent = tentativas;
+
+    var listajogadores = JSON.parse(localStorage.getItem('listajogadores')) || [];
+    var listadejogadores = document.getElementById('listadejogadores');
+
+    listajogadores.forEach(function(entry) {
+        var nomejogador = document.createElement('li');
+        nomejogador.textContent = `${entry.nome}: ${entry.tentativa} tentativas`;
+        listadejogadores.appendChild(nomejogador);
+    });
+
+    var botao = document.getElementById('botao');
+    botao.addEventListener("click", jogarNovamente);
+
+    
+    document.addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            jogarNovamente();
+        }
+    });
+
+    function jogarNovamente() {
+        localStorage.removeItem('listajogadores'); // Limpa os dados de jogadores
+        localStorage.removeItem('tentativas');    // Limpa as tentativas
+        window.location.href = "file:///C:/Users/motog/Downloads/jogo/formulario/index.html"; // volta pro inicio
+    }
+});
